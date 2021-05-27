f2f154ced08572f41ec1a18c57564c55de0f6385
nabilgj <nabilgj.bnm@gmail.com>
2021-05-27 11:19:37 -0400

    setup production webpack build to extract css into bundle

--- src/app.js
+++ src/app.js
@@ -8,13 +8,12 @@ import "react-dates/lib/css/_datepicker.css";
 import AppRouter from "./routers/AppRouter";
 
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
-
-import getVisisbleExpenses from "./selectors/expenses";
 
 import { Provider } from "react-redux";
 
 const store = configureStore();
+
+console.log("coming from app testing");
 
 const jsx = (
   <Provider store={store}>
